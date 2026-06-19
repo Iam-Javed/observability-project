3e982cdcf7f5dc001fae7215dfea87db261dea60
Ubuntu <ubuntu@ip-172-31-89-198.ec2.internal>
2026-06-19 06:14:34 +0000

    Updated code for flask app

--- app/app.py
+++ app/app.py
@@ -4,9 +4,24 @@ import logging
 import random
 import time
 
+# OpenTelemetry imports
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
+# ---------------- Logging ----------------
+
+logging.basicConfig(
+    filename='/logs/app.log',
+    level=logging.INFO
+)
+
+# ---------------- Metrics ----------------
 
 REQUEST_COUNT = Counter(
     'http_requests_total',
@@ -18,40 +33,50 @@ REQUEST_TIME = Histogram(
     'Request duration'
 )
 
+# ---------------- Tracing ----------------
+
+resource = Resource.create(
+    {"service.name": "flask-app"}
+)
+
+trace.set_tracer_provider(
+    TracerProvider(resource=resource)
+)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://jaeger:4318/v1/traces"
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+FlaskInstrumentor().instrument_app(app)
+
+# ---------------- Routes ----------------
 
 @app.route("/")
+@REQUEST_TIME.time()
 def home():
-
-    start = time.time()
-
-    logging.info("Home endpoint accessed")
-
-    delay = random.uniform(0.2,2)
-
-    time.sleep(delay)
 
     REQUEST_COUNT.inc()
 
-    REQUEST_TIME.observe(time.time()-start)
+    sleep_time = random.uniform(0.2,1)
 
-    return "Application running"
+    time.sleep(sleep_time)
 
+    logging.info("Home page accessed")
 
-@app.route("/error")
-def error():
-
-    logging.error("Error endpoint hit")
-
-    return "Internal Error",500
+    return "Observability Project"
 
 
-@app.route("/metrics")
+@app.route('/metrics')
 def metrics():
-
     return Response(
         generate_latest(),
         mimetype=CONTENT_TYPE_LATEST
     )
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000)
